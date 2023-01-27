64bf37c06d98a1f11aa2b4ca9d397ab76f4ceec1
Alex Canez <el.winy.36@gmail.com>
2023-01-27 16:41:16 -0700

    new commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+// function greet(name, lastName) {
+//     console.log('hello ' + name + ' ' + lastName);
+// }
+
+// greet('alex' , 'canez');
+
+for (let i = 5; i >= 1; i --) {
+    if (i % 2 !==0)console.log(i);
+}
