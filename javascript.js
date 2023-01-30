8818fdddd123c29f4f1401daefb2779b27d222df
Alex Canez <el.winy.36@gmail.com>
2023-01-30 15:20:35 -0700

    added work

--- javascript.js
+++ javascript.js
@@ -5,6 +5,10 @@
 
 // greet('alex' , 'canez');
 
-for (let i = 5; i >= 1; i --) {
-    if (i % 2 !==0)console.log(i);
-}
+// for (let i = 5; i >= 1; i --) {
+//     if (i % 2 !==0)console.log(i);
+// }
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
