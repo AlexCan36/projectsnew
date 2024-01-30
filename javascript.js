a7b475be6b22428c46ed02e2ad2875d346715c82
Alex Canez <el.winy.36@gmail.com>
2024-01-29 19:02:15 -0700

    first python project

--- javascript.js
+++ javascript.js
@@ -12,3 +12,4 @@
 let selectedColors = ['red', 'blue'];
 selectedColors[2] = 1;
 console.log(selectedColors.length);
+
---
+++ proyectos.py
@@ -0,0 +1,8 @@
+x = 2
+y = 2
+res = x + y
+y = 5
+res = y + x
+print(res)
+
+ 
